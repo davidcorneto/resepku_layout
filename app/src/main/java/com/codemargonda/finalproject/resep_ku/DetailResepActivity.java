ca3cf9484135db46258137c79261ce550664d1d3
davidcorneto <davidcornelius15@gmail.com>
2016-10-02 12:45:24 +0700

    layout xml

---
+++ app/src/main/java/com/codemargonda/finalproject/resep_ku/DetailResepActivity.java
@@ -0,0 +1,13 @@
+package com.codemargonda.finalproject.resep_ku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailResepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_resep);
+    }
+}
---
+++ app/src/main/java/com/codemargonda/finalproject/resep_ku/TambahResepActivity.java
@@ -0,0 +1,13 @@
+package com.codemargonda.finalproject.resep_ku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TambahResepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_resep);
+    }
+}
